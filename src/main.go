bfc7fe43a10a8981fcead7750058bbd5bd5ab1bd
Conrado Guerra <conradoguerra@gmail.com>
2024-09-11 22:21:24 +0200

    Create estimation route

--- src/main.go
+++ src/main.go
@@ -1,22 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"energy_estimation/src/domain/historic_consumption"
 	"energy_estimation/src/services"
 	"fmt"
+	"io"
+	"net/http"
 )
+
+type HistoricConsumptionDTO struct {
+	Client_Id string       `json:"client_id"`
+	Measures  []MeasureDTO `json:"measures"`
+}
+
+type MeasureDTO struct {
+	Consumption uint16 `json:"consumption"`
+	Begin       string `json:"begin"`
+	End         string `json:"end"`
+}
+
+func EstimationHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+	}
+
+	var historicConsumptionDTO HistoricConsumptionDTO
+	json.Unmarshal(body, &historicConsumptionDTO)
+
+	historicConsumption := convertToDomain(historicConsumptionDTO)
+
+	estimation, err := services.NewEstimationService(&historicConsumption)
+
+	if err != nil {
+		http.Error(w, "Error calculating estimation", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	jsonResponse, err := json.Marshal(estimation)
+	if err != nil {
+		http.Error(w, "Error creating response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResponse)
+
+}
+
+func convertToDomain(dto HistoricConsumptionDTO) historic_consumption.HistoricConsumption {
+	measures := make([]historic_consumption.Measure, len(dto.Measures))
+	for i, measure := range dto.Measures {
+		measures[i] = historic_consumption.Measure{Begin: measure.Begin, End: measure.End, Consumption: measure.Consumption}
+	}
+	return historic_consumption.HistoricConsumption{
+		Client_Id: dto.Client_Id,
+		Measures:  measures,
+	}
+
+}
 
 func main() {
 
-	var historic = historic_consumption.HistoricConsumption{
-		Client_Id: "historic_pdl",
-		Measures: []historic_consumption.Measure{
-			{Consumption: 23, Begin: "2024/09/01", End: "2024/09/30"},
-			{Consumption: 4, Begin: "2024/08/01", End: "2024/08/31"},
-			{Consumption: 54, Begin: "2024/07/01", End: "2024/07/31"}},
-	}
-
-	estimation, _ := services.NewEstimationService(&historic)
-	fmt.Print(estimation)
+	http.HandleFunc("/api/estimation", EstimationHandler)
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
 
 }
